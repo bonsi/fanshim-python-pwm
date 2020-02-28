afa0ec75e4881c7fc8a3c10b50c5c3319c8535c2
grayerbeard <feedback@smalle.uk>
2020-02-28 13:26:04 +0000

    adjusting thresholds

--- library/fanshim/__init__.py
+++ library/fanshim/__init__.py
@@ -35,6 +35,7 @@ class FanShim():
         #GPIO.setup(self._pin_fancontrol, GPIO.OUT)
         #GPIO.setup(self._pin_button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         
+        print("hollo")
 
         #PWM Version
         GPIO.setwarnings(False)
